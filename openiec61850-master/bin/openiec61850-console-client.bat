49fbe05e685e19b7b01186250ab34908194d1e43
mandybandy <mandl150034@sr.htlweiz.at>
2020-02-19 14:31:51 +0100

    added string list and created new lib

--- openiec61850-master/bin/openiec61850-console-client.bat
+++
@@ -1,8 +0,0 @@
-::BATCH file for windows
-
-set BATDIR=%~dp0
-set LIBDIR=%BATDIR%..\build\libs-all
-
-java  -Djava.ext.dirs=%LIBDIR% com.beanit.openiec61850.app.ConsoleClient %*
-
-set /p banane= taste druecken um fortzufahren:
